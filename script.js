2e006f49cb02b9e300e194a1b4e519869cca72bb
Shubham Singh <shubhamsng7@gmail.com>
2021-04-02 19:26:53 +0530

    update progress

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const people = [
         {
             name: "Saurabh Singh",
-            progress: 25000,
+            progress: 14000,
             goal: 1070000,
             img: 'https://pbs.twimg.com/profile_images/1117457128149544960/aXLwSzcB_400x400.jpg'
         },
         {
             name: "Shubham Singh",
-            progress: 40000,
+            progress: 50000,
             goal: 250000,
             img: 'https://pbs.twimg.com/profile_images/1346432398955741186/vghJ7OFG_400x400.jpg'
         },
         {
             name: "Sushant Singh",
-            progress: 20000,
+            progress: 15600,
             goal: 1000000,
             img: 'https://pbs.twimg.com/profile_images/1306644761411899393/jlOj9vP__400x400.jpg'
         }
@@ -29,11 +29,15 @@ document.addEventListener('DOMContentLoaded', () => {
     daysLeft.innerHTML = days + " days left"
     document.body.appendChild(daysLeft);
 
+    const mainDiv = document.createElement('div');
+    mainDiv.className = "row";
+    document.body.appendChild(mainDiv);
+
     for(let i = 0, l = people.length; i < l; i++)
     {
         const card = document.createElement('div');
         card.className = 'card';
-        document.body.appendChild(card);
+        mainDiv.appendChild(card);
         
         const img = document.createElement('img');
         img.className = 'image';
@@ -61,5 +65,42 @@ document.addEventListener('DOMContentLoaded', () => {
         card.appendChild(progress);
 
     }
+
+    const progress = document.createElement('meter');
+    progress.addEventListener('hover', () => {
+
+    });
+    // for(let i = 0, l = people.length; i < l; i++)
+    // {
+    //     const card = document.createElement('div');
+    //     card.className = 'card';
+    //     document.body.appendChild(card);
+        
+    //     const img = document.createElement('img');
+    //     img.className = 'image';
+    //     img.setAttribute('src', people[i].img);
+    //     card.appendChild(img);
+
+    //     const name = document.createElement('h2');
+    //     name.className = 'name';
+    //     name.innerHTML = people[i].name;
+    //     card.appendChild(name);
+
+    //     const goal = document.createElement('h3');
+    //     goal.className = 'goal';
+    //     goal.innerHTML = "₹ " + people[i].goal.toLocaleString();
+    //     card.appendChild(goal);
+
+    //     const progress = document.createElement('meter');
+    //     progress.className = 'progress';
+    //     progress.setAttribute('min', 0);
+    //     progress.setAttribute('max', people[i].goal);
+    //     progress.setAttribute('value', people[i].progress);
+    //     progress.setAttribute('low', (people[i].goal /100) * 30);
+    //     progress.setAttribute('high', (people[i].goal /100) * 70);
+    //     progress.setAttribute('optimum', (people[i].goal /100) * 80);
+    //     card.appendChild(progress);
+
+    // }
 
 });
